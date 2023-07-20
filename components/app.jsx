1ab75ff35c6f763ead8d205e5a80e9069db9728a
kevin <flourzeez@gmail.com>
2023-07-20 11:03:34 +0100

    added new components

--- components/app.jsx
+++ components/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import Note from "./note";
 
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
   <div>
     <Header />
+    <Note />
     <Footer />
   </div>
   );
